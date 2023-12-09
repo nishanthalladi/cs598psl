2449e9a3bab14e7f59bd532626a366c74ee35dc6
nishanthalladi <nishanthalladi01@gmail.com>
2023-12-08 19:42:11 -0500

    steps 2 and 3 done

---
+++ proj3/my_main_with_recording.R
@@ -0,0 +1,62 @@
+#!/usr/bin/env RScript
+### Taking in args 
+library("stopwords")
+library(text2vec)
+library("pROC")
+library(glmnet)
+
+args = commandArgs(trailingOnly=TRUE)
+myvocab = scan(args[1], character(), quote = "")
+train = read.table(args[2],
+                   stringsAsFactors = FALSE,
+                   header = TRUE)
+test = read.table(args[3],
+                  stringsAsFactors = FALSE,
+                  header = TRUE)
+
+###REMOVE
+start_time = Sys.time()
+###REMOVE
+
+all_reviews = c(train$review)
+all_sentiments = train[['sentiment']]
+it_train = itoken(all_reviews,
+                  preprocessor = tolower, 
+                  tokenizer = word_tokenizer)
+vectorizer = vocab_vectorizer(create_vocabulary(myvocab, 
+                                                ngram = c(1L, 2L)))
+dtm_train_new = create_dtm(it_train, vectorizer)
+set.seed(1)
+
+glmnet_classifier = cv.glmnet(x = dtm_train_new, y = all_sentiments,
+                              family = 'binomial',
+                              alpha = 0,
+                              type.measure = "auc",
+                              nfolds = 4,
+                              thresh = 1e-3,
+                              maxit = 1e3)
+
+it_test = word_tokenizer(tolower(test$review))
+it_test = itoken(it_test, ids = test$id)
+
+dtm_test = create_dtm(it_test, vectorizer)
+
+preds = predict(glmnet_classifier, dtm_test, type = 'response')[,1]
+
+preds_df = data.frame(unlist(names(preds)), unlist(preds))
+names(preds_df) = c('id', 'prob')
+
+###REMOVE
+cat(Sys.time() - start_time)
+###REMOVE
+
+file_path = paste0('mypred.csv')
+readr::write_csv(as.data.frame(preds_df), file_path)
+
+###REMOVE
+test_y = read.table(args[4],
+                    stringsAsFactors = FALSE,
+                    header = TRUE)
+auc = glmnet:::auc(test_y$sentiment, preds)
+print(auc)
+###REMOVE
--- proj3/mymain.R
+++ proj3/mymain.R
@@ -4,34 +4,18 @@ library("stopwords")
 library(text2vec)
 library("pROC")
 library(glmnet)
-#TODO uncomment this for final part
-#args = commandArgs(trailingOnly=TRUE)
-#TODO update myvocab and read in whatever format you output in
-#myvocab = read.csv(args[1], stringsAsFactors = FALSE) 
-# train = read.table(args[2],
-#                    stringsAsFactors = FALSE,
-#                    header = TRUE)
-# test = read.table(args[3],
-#                   stringsAsFactors = FALSE,
-#                   header = TRUE)
 
-#TODO remove as this part is only for testing, use above code for command line
-train = read.table('train.tsv',
+args = commandArgs(trailingOnly=TRUE)
+myvocab = scan(args[1], character(), quote = "")
+train = read.table(args[2],
                    stringsAsFactors = FALSE,
                    header = TRUE)
-test = read.table('test.tsv',
+test = read.table(args[3],
                   stringsAsFactors = FALSE,
                   header = TRUE)
-train$review = gsub('&lt;.*?&gt;', ' ', train$review)
-test$review = gsub('&lt;.*?&gt;', ' ', test$review)
-
 
 all_reviews = c(train$review)
-
-
 all_sentiments = train[['sentiment']]
-
-
 it_train = itoken(all_reviews,
                   preprocessor = tolower, 
                   tokenizer = word_tokenizer)
@@ -57,8 +41,6 @@ preds = predict(glmnet_classifier, dtm_test, type = 'response')[,1]
 
 preds_df = data.frame(unlist(names(preds)), unlist(preds))
 names(preds_df) = c('id', 'prob')
+
 file_path = paste0('mypred.csv')
 readr::write_csv(as.data.frame(preds_df), file_path)
-#TODO remove
-auc = glmnet:::auc(test_y$sentiment, preds)
-print(auc)
